8c1ea062914031582cfba29c2fcdb9b5f52d856d
czp3009 <czp3009@gmail.com>
2017-06-24 13:15:23 +0800

    修正弹幕回复实体中的预设定code不是静态的问题

--- src/main/java/com/hiczp/bilibili/live/danmu/api/entity/DanMuResponseEntity.java
+++ src/main/java/com/hiczp/bilibili/live/danmu/api/entity/DanMuResponseEntity.java
@@ -7,10 +7,10 @@ import com.alibaba.fastjson.JSONArray;
  * Created by czp on 17-6-6.
  */
 public class DanMuResponseEntity {
-    public int NO_LOGIN = -101;
-    public int WRONG_PARAM = -400;
-    public int OUT_OF_LENGTH = -500;
-    public int SUCCESS = 0;
+    public static int NO_LOGIN = -101;
+    public static int WRONG_PARAM = -400;
+    public static int OUT_OF_LENGTH = -500;
+    public static int SUCCESS = 0;
 
     /**
      * Result code of sending DanMu.
